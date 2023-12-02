457b670bd0fa4175292fe0dab3343bb94f6bd61d
hussein <hussein.merehby@gmail.com>
2023-12-03 01:29:12 +0200

    2nd

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ const PORT = process.env.PORT || 3000;
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-
+app.get('/students', function(req, res){
+  var data = [
+    {id:1, name:'cezar'},
+    {id:2, name:'mhmd'},
+    {id:3, name:'rony'},
+    {id:4, name:'hicham'}
+  ]
+  res.send(data)
+})
 app.listen(PORT)
